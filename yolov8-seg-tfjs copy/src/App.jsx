b5eced9c3e26c09a2ca92be17f07ceb138b32984
bladerunner1911 <130647892+bladerunner1911@users.noreply.github.com>
2023-05-26 02:23:47 +0700

    Add files via upload

---
+++ yolov8-seg-tfjs copy/src/App.jsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect, useRef } from "react";
+import * as tf from "@tensorflow/tfjs";
+import "@tensorflow/tfjs-backend-webgl"; // set backend to webgl
+import Loader from "./components/loader";
+import ButtonHandler from "./components/btn-handler";
+import { detectFrame, detectVideo } from "./utils/detect";
+import "./style/App.css";
+
+const App = () => {
+  const [loading, setLoading] = useState({ loading: true, progress: 0 }); // loading state
+  const [model, setModel] = useState({
+    net: null,
+    inputShape: [1, 0, 0, 3],
+  }); // init model & input shape
+
+  // references
+  const imageRef = useRef(null);
+  const cameraRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  // model configs
+  const modelName = "yolov5n"; // ten cua model
+  const classThreshold = 0.2;
+
+  useEffect(() => {
+    tf.ready().then(async () => {
+      const yolov8 = await tf.loadGraphModel(
+        `${window.location.href}/${modelName}_web_model/model.json`,  // duong dan cua model
+        {
+          onProgress: (fractions) => {
+            setLoading({ loading: true, progress: fractions }); // set loading fractions
+          },
+        }
+      ); // load model
+
+      // warming up model
+      const dummyInput = tf.randomUniform(yolov8.inputs[0].shape, 0, 1, "float32"); // random input
+      const warmupResults = yolov8.execute(dummyInput);
+
+      setLoading({ loading: false, progress: 1 });
+      setModel({
+        net: yolov8,
+        inputShape: yolov8.inputs[0].shape,
+        outputShape: warmupResults.map((e) => e.shape),
+      }); // set model & input shape
+
+      tf.dispose([warmupResults, dummyInput]); // cleanup memory
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      {loading.loading && <Loader>Loading model... {(loading.progress * 100).toFixed(2)}%</Loader>}
+      <div className="header">
+        <h1>📷 YOLOv8 Live Segmentation App</h1>
+        <p>
+          YOLOv8 live detection application on browser powered by <code>tensorflow.js</code>
+        </p>
+        <p>
+          Serving : <code className="code">{modelName}</code>
+        </p>
+      </div>
+
+      <div className="content">
+        <img
+          src="#"
+          ref={imageRef}
+          onLoad={() => detectFrame(imageRef.current, model,classThreshold,canvasRef.current)}
+        />
+        <video
+          autoPlay
+          muted
+          ref={cameraRef}
+          onPlay={() => detectVideo(cameraRef.current, model,classThreshold,canvasRef.current)}
+        />
+        <canvas width={model.inputShape[2]} height={model.inputShape[1]} ref={canvasRef} />
+      </div>
+
+      <ButtonHandler imageRef={imageRef} cameraRef={cameraRef} />
+    </div>
+  );
+};
+
+export default App;
